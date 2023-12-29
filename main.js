b695d37f8a8a13c1d2ef565741bd91f68e34aef7
Mateusz <mateusz.moqbil@gmail.com>
2023-12-30 00:15:06 +0100

    All functions almost works

--- main.js
+++ main.js
@@ -33,15 +33,15 @@ const createTable = (data) => {
   const table = document.createElement("table");
   table.style.border = "3px solid";
 
-  let keysDisplayed = false;
+  let areKeysDisplayed = false;
   let displayKeyIndex = 0;
   let keyId = 1;
-  const maxDisplayedIndex = 6;
+  const maxDisplayedKeyIndex = 6;
 
   for (const key in data) {
     const props = data[key];
 
-    if (!keysDisplayed) {
+    if (!areKeysDisplayed) {
       const trKey = document.createElement("tr");
       trKey.style.border = "1px solid";
       table.appendChild(trKey);
@@ -52,7 +52,7 @@ const createTable = (data) => {
       trKey.appendChild(tdKeyId);
 
       for (const propKey in props) {
-        if (displayKeyIndex % 2 === 0 && displayKeyIndex < maxDisplayedIndex) {
+        if (displayKeyIndex % 2 === 0 && displayKeyIndex < maxDisplayedKeyIndex) {
           const tdKey = document.createElement("td");
           tdKey.innerHTML = propKey.toUpperCase();
           tdKey.style.border = "1px solid";
@@ -71,7 +71,7 @@ const createTable = (data) => {
       tdKeyActions.style.border = "1px solid";
       trKey.appendChild(tdKeyActions);
 
-      keysDisplayed = true;
+      areKeysDisplayed = true;
     }
 
     displayKeyIndex = 0;
@@ -89,7 +89,7 @@ const createTable = (data) => {
         keyId++;
       }
 
-      if (displayKeyIndex % 2 === 0 && displayKeyIndex < 5) {
+      if (displayKeyIndex % 2 === 0 && displayKeyIndex < maxDisplayedKeyIndex) {
         if (propValue) {
           const tdValue = document.createElement("td");
           tdValue.innerHTML = propValue;
@@ -102,8 +102,11 @@ const createTable = (data) => {
         const tdCreatedValue = document.createElement("td");
         tdCreatedValue.style.border = "1px solid";
         const dateObject = new Date(props["created"]);
-        const day = (dateObject.getDate() < 10 ? "0" : "") + dateObject.getDate();
-        const month = ((dateObject.getMonth() + 1) < 10 ? "0" : "") + (dateObject.getMonth() + 1);
+        const day =
+          (dateObject.getDate() < 10 ? "0" : "") + dateObject.getDate();
+        const month =
+          (dateObject.getMonth() + 1 < 10 ? "0" : "") +
+          (dateObject.getMonth() + 1);
         const formattedDate = `${day}-${month}-${dateObject.getFullYear()}`;
         tdCreatedValue.innerHTML = formattedDate;
         trValue.appendChild(tdCreatedValue);
@@ -130,13 +133,14 @@ const createTable = (data) => {
   return table;
 };
 
+
+
 const createTableButtons = () => {
   const tableButtons = document.createElement("td");
   const buttonPrev = createButton("Prev", () => {});
   const buttonNext = createButton("Next", () => {});
   const tableInput = createInput("1");
-
-  const selectElement = document.createElement("select");
+  tableInput.value = 1;
 
   const option1 = document.createElement("option");
   option1.value = "10";
@@ -146,15 +150,86 @@ const createTableButtons = () => {
   option2.value = "20";
   option2.text = "20";
 
+  const selectElement = document.createElement("select");
+
   selectElement.add(option1);
   selectElement.add(option2);
+
+  selectElement.value = "10";
+
+  selectElement.addEventListener("change", (event) => {
+    const selectedValue = parseInt(event.target.value);
+    const inputValue = parseInt(tableInput.value);
+    updateTableRows(inputValue, selectedValue);
+
+    const maxPages = calculateMaxPages(selectedValue);
+    if(selectedValue == 10) {
+      updateTableRows(inputValue*2-1, 10);
+      tableInput.value = inputValue*2-1;
+    }
+    else if(selectedValue == 20) {
+      updateTableRows(Math.ceil(inputValue/2), 20);
+      tableInput.value = Math.ceil(inputValue/2);
+    }
+    else if (inputValue > maxPages) {
+      tableInput.value = maxPages;
+      updateTableRows(tableInput.value , selectedValue);
+    }
+  });
 
   tableButtons.appendChild(buttonPrev);
   tableButtons.appendChild(tableInput);
   tableButtons.appendChild(buttonNext);
   tableButtons.appendChild(selectElement);
 
+
+  tableInput.addEventListener("input", (event) => {
+    const inputValue = parseInt(event.target.value);
+    const maxPages = calculateMaxPages(selectElement.value);
+    const selectedValue = parseInt(selectElement.value);
+    if (inputValue < 1) {
+      event.target.value = 1;
+    } else if (inputValue > maxPages) {
+      event.target.value = maxPages;
+    }
+    updateTableRows(event.target.value, selectedValue);
+    
+    
+  });
+
+  tableInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const inputValue = parseInt(tableInput.value);
+      const selectedValue = parseInt(selectElement.value);
+      updateTableRows(inputValue, selectedValue);
+    }
+  });
+
+  updateTableRows(1, 10);
+
   return tableButtons;
+};
+
+const calculateMaxPages = (selectElementValue) => {
+  const rowsPerPage = parseInt(selectElementValue);
+  const totalRows = mainTable.querySelectorAll("tr").length - 1; 
+  return Math.ceil(totalRows / rowsPerPage);
+};
+
+const updateTableRows = (inputValue, selectedValue) => {
+  const rows = mainTable.querySelectorAll("tr");
+
+  rows.forEach((row, index) => {
+    if (index === 0) {
+      return;
+    }
+
+    if (index >= (inputValue - 1) * selectedValue + 1 && index <= inputValue * selectedValue) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
 };
 
 const createDeleteButton = () => {
@@ -175,60 +250,60 @@ const createDeleteButton = () => {
 };
 
 const showModal = (data) => {
-    if (!isModalOpen) {
-      isModalOpen = true;
-      const modal = document.createElement("div");
-      modal.style.position = "fixed";
-      modal.style.top = "50%";
-      modal.style.left = "50%";
-      modal.style.transform = "translate(-50%, -50%)";
-      modal.style.backgroundColor = "white";
-      modal.style.padding = "20px";
-      modal.style.border = "2px solid yellow";
-      modal.style.zIndex = "1000";
-      modal.style.maxWidth = "80%";
-  
-      const buttonX = createButton("X", () => {
-        isModalOpen = false;
-        content.removeChild(modal);
-      });
-      modal.appendChild(buttonX);
-      content.appendChild(modal);
-  
-      const tableModal = document.createElement("table");
-      tableModal.style.width = "100%";
-      tableModal.style.overflowX = "auto";
-      modal.appendChild(tableModal);
-  
-      for (const propKey in data) {
-        const propValue = data[propKey];
-        const trModal = document.createElement("tr");
-        const tdKeyModal = document.createElement("td");
-        const tdValueModal = document.createElement("td");
-  
-        tdKeyModal.style.maxWidth = "1000px";
-        tdKeyModal.style.overflow = "hidden";
-        tdKeyModal.style.textOverflow = "ellipsis";
-        tdKeyModal.style.whiteSpace = "nowrap";
-  
-        tdValueModal.style.maxWidth = "1000px";
-        tdValueModal.style.overflow = "hidden";
-        tdValueModal.style.textOverflow = "ellipsis";
-        tdValueModal.style.whiteSpace = "nowrap";
-  
-        tdKeyModal.innerHTML = propKey;
-        tdValueModal.innerHTML = propValue;
-        trModal.appendChild(tdKeyModal);
-        trModal.appendChild(tdValueModal);
-        tableModal.appendChild(trModal);
-      }
-  
-      buttonX.addEventListener("click", function () {
-        isModalOpen = false;
-        content.removeChild(modal);
-      });
+  if (!isModalOpen) {
+    isModalOpen = true;
+    const modal = document.createElement("div");
+    modal.style.position = "fixed";
+    modal.style.top = "50%";
+    modal.style.left = "50%";
+    modal.style.transform = "translate(-50%, -50%)";
+    modal.style.backgroundColor = "white";
+    modal.style.padding = "20px";
+    modal.style.border = "2px solid yellow";
+    modal.style.zIndex = "1000";
+    modal.style.maxWidth = "80%";
+
+    const buttonX = createButton("X", () => {
+      isModalOpen = false;
+      content.removeChild(modal);
+    });
+    modal.appendChild(buttonX);
+    content.appendChild(modal);
+
+    const tableModal = document.createElement("table");
+    tableModal.style.width = "100%";
+    tableModal.style.overflowX = "auto";
+    modal.appendChild(tableModal);
+
+    for (const propKey in data) {
+      const propValue = data[propKey];
+      const trModal = document.createElement("tr");
+      const tdKeyModal = document.createElement("td");
+      const tdValueModal = document.createElement("td");
+
+      tdKeyModal.style.maxWidth = "1000px";
+      tdKeyModal.style.overflow = "hidden";
+      tdKeyModal.style.textOverflow = "ellipsis";
+      tdKeyModal.style.whiteSpace = "nowrap";
+
+      tdValueModal.style.maxWidth = "1000px";
+      tdValueModal.style.overflow = "hidden";
+      tdValueModal.style.textOverflow = "ellipsis";
+      tdValueModal.style.whiteSpace = "nowrap";
+
+      tdKeyModal.innerHTML = propKey;
+      tdValueModal.innerHTML = propValue;
+      trModal.appendChild(tdKeyModal);
+      trModal.appendChild(tdValueModal);
+      tableModal.appendChild(trModal);
     }
-  };
+
+    buttonX.addEventListener("click", function () {
+      isModalOpen = false;
+      content.removeChild(modal);
+    });
+  }
+};
 
 const filterTableByName = (nameString) => {
   const rows = mainTable.querySelectorAll("tr");
@@ -277,9 +352,17 @@ const initTable = (key) => {
   const buttonsObject = rowData[key];
 
   searchByIdInput = createInput(`1 - ${buttonsObject.length}`);
-  const searchByIdDiv = createDiv("Search by Id", searchByIdInput, createButton("Search", () => filterTableById(parseInt(searchByIdInput.value))));
+  const searchByIdDiv = createDiv(
+    "Search by Id",
+    searchByIdInput,
+    createButton("Search", () =>
+      filterTableById(parseInt(searchByIdInput.value))
+    )
+  );
 
-  searchByNameInput = createInput(key === "films" ? "Search by title" : "Search by name");
+  searchByNameInput = createInput(
+    key === "films" ? "Search by title" : "Search by name"
+  );
   const searchByNameDiv = createDiv(
     key === "films" ? "Search by title" : "Search by name",
     searchByNameInput,
@@ -300,10 +383,15 @@ const initTable = (key) => {
 
   document.addEventListener("change", function () {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    const atLeastOneChecked = Array.from(checkboxes).some((checkbox) => checkbox.checked);
+    const atLeastOneChecked = Array.from(checkboxes).some(
+      (checkbox) => checkbox.checked
+    );
     deleteButton.style.display = atLeastOneChecked ? "block" : "none";
   });
 };
+
+
+
 
 const initUI = () => {
   Object.keys(rowData).forEach((key) => {
